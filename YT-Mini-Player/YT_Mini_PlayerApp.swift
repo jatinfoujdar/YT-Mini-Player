d38d76cf4fd579e3dc84aa7aff4da307bce0f3a5
Jatin Foujdar <jatin321001@gmail.com>
2024-12-29 22:09:40 +0530

    Initial Commit

---
+++ YT-Mini-Player/YT_Mini_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_Mini_PlayerApp.swift
+//  YT-Mini-Player
+//
+//  Created by jatin foujdar on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct YT_Mini_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
